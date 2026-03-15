cf019812f8ccc8c6b00c7f89ac17a8b6b7e51102
Quentin Male <qmale@seas.harvard.edu>
2026-03-15 18:05:39 -0400

    update run.sh cannon

--- basilisk/3d/run.sh
+++ basilisk/3d/run.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 2-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p seas_compute
 #SBATCH --mem=0           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --ntasks=32
+#SBATCH --ntasks=112
 #SBATCH --constraint sapphirerapids
 #SBATCH --cpus-per-task=1
 ##SBATCH --exclusive
@@ -15,7 +15,7 @@ mkdir -p run
 mkdir -p output
 mkdir -p checkpoints
 
-CC99=mpicc ./qcc -D_GNU_SOURCE -D_MPI=1 -O2 -disable-dimensions sharp_crested_weir_3d.c -o exec -lm
+CC99=mpicc ./qcc -D_GNU_SOURCE -D_MPI=1 -DJACOBI -O2 -disable-dimensions sharp_crested_weir_3d.c -o exec -lm
 
 export OMPI_MCA_coll_hcoll_enable=0
 export OMPI_MCA_coll=^hcoll,ml
